d1fd0d136b217bd6cfb89cc97b363e7670fa6c8b
claudiacole96 <claudia.evans@bryanuniversity.edu>
2021-04-06 20:44:05 -0400

    week3

--- week3/intro-to-redux/src/components/TimerReducer.js
+++ week3/intro-to-redux/src/components/TimerReducer.js
@@ -6,122 +6,98 @@ const initialState = {
     started: false,
     paused: false,
     laps: []
-  }
+}
   
-  export function timer() {
+export function timer() {
     return {
         type: "TIMER"
     }
-  
-  }
-  
-  export function start() {
+}
+export function start() {
     return {
         type: "START"
     }
-  }
-  
-  export function stop() {
+} 
+export function stop() {
     return {
         type: "STOP"
     }
-  }
-  
-  export function lap() {
+}
+export function lap() {
     return {
         type: "LAP"
     }
-  }
-  
-  export function reset() {
+}
+export function reset() {
     return {
         type: "RESET"
     }
-  }
+}
   
-  function TimerReducer(state = initialState, action) {
-  
-    if (action.type === 'TIMER') {
-        
-        let time = state.time
-        let miliseconds = Date.now() - state.time
-        let seconds = state.seconds
-        let minutes = state.minutes
-  
-        if(state.paused){
-            time = new Date(state.time - state.miliseconds)
-            miliseconds = Date.now() - time
-        }
-  
-        if(miliseconds >= 1000)
-        {
-            seconds += 1   
-            time = new Date()
-        }
-  
-        if(seconds >= 60) {
-            seconds = 0
-            minutes += 1
-        }
-  
-        return {
-            ...state,
-            time: time,
-            miliseconds: miliseconds,
-            seconds: seconds,
-            minutes: minutes,
-            paused: false
-        }
+function TimerReducer(state = initialState, action) {
+    switch(action.type) {
+        case "TIMER":
+            let time = state.time
+            let miliseconds = Date.now() - state.time
+            let seconds = state.seconds
+            let minutes = state.minutes
+            if (state.paused) {
+                time = new Date(state.time - state.miliseconds)
+                miliseconds = Date.now() - time
+            }
+            if (miliseconds >= 1000) {
+                seconds += 1
+                time = new Date()
+            }
+            if (seconds >= 60) {
+                seconds = 0
+                minutes += 1
+            }
+            return {
+                ...state,
+                time: time,
+                miliseconds: miliseconds,
+                seconds: seconds,
+                minutes: minutes,
+                paused: false
+            }
+        case "START":
+            let newTime = new Date()
+            return {
+                ...state,
+                time: newTime,
+                started: true
+            }
+        case "STOP":
+            return {
+                ...state,
+                started: false,
+                paused: true
+            }
+        case "LAP":
+            let lap = {
+                "minutes": state.minutes,
+                "seconds": state.seconds,
+                "miliseconds": state.miliseconds
+            }
+            return {
+                ...state,
+                laps: [...state.laps, lap]
+            }
+        case "RESET":
+            return {
+                ...state,
+                time: new Date(),
+                minutes: 0,
+                seconds: 0,
+                miliseconds: 0,
+                start: false,
+                pause: false,
+                laps: []
+            }
+        default:
+            return state
     }
+}
   
-    if (action.type === 'START') {
-  
-        let newTime = new Date()
-        return {
-            ...state,
-            time: newTime,
-            started: true,
-        }
-  
-    }
-  
-    if (action.type === 'STOP') {
-        return {
-            ...state,
-            started: false,
-            paused: true
-        }
-    }
-  
-    if (action.type === 'LAP') {
-        
-        let lap = {
-            "minutes": state.minutes,
-            "seconds": state.seconds,
-            "miliseconds": state.miliseconds
-        }
-  
-        return {
-            ...state,
-            laps: [...state.laps, lap]
-        }
-    }
-  
-    if (action.type === 'RESET') {
-        return {
-            ...state,
-            time: new Date(),
-            minutes: 0,
-            seconds: 0,
-            miliseconds: 0,
-            start: false,
-            pause: false,
-            laps: []
-        }
-    }
-  
-    return state;
-  
-  }
-  
-  export default TimerReducer
+export default TimerReducer
