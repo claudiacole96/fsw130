ab3d542dc59ad807f199e537ada1410a9ae30909
claudiacole96 <claudia.evans@bryanuniversity.edu>
2021-04-07 21:13:49 -0400

    week4

---
+++ week4/redux-contacts/app.js
@@ -0,0 +1,100 @@
+/* Build a contact management system using Redux in a vanilla JavaScript environment (no React).
+
+You should be able to dispatch actions that add a new person and remove an existing person from an array of contacts.
+
+The contact objects can have whatever properties you want. For example: */
+
+const redux = require("redux")
+
+let contactState = {
+    contacts: []
+}
+
+const addContact = () => {
+    return {
+        type: "ADDCONTACT",
+        payload: person
+    }
+}
+const deleteContact = () => {
+    return {
+        type: "DELETECONTACT",
+        payload: person
+    }
+}
+
+function reducer(state = contactState, action) {
+    switch (action.type) {
+        case "ADDCONTACT":
+            let arr = state.state.contacts
+            arr.push(action.payload)
+            return {
+                state: {
+                    contacts: arr,
+                    newContact: action.payload
+                }
+            }
+        case "DELETECONTACT":
+            let oldArr = state.state.contacts
+            let x = parseInt(action.payload)
+            let newArr = oldArr.filter(person => person.id !== x)
+            return {
+                state: {
+                    contacts: newArr
+                }
+            }
+        default:
+            return {
+                state
+            }
+    }
+}
+
+const store = redux.createStore(reducer)
+
+function renderWebPage() {
+    let state = store.getState()
+    const contactDiv = document.getElementById("contact-parent")
+    let x = state.state.newContact
+    console.log("in the render", state.state.contacts)
+    let div = document.createElement("div")
+    let h3 = document.createElement("h3")
+    let h4email = document.createElement("h4")
+    let h4phone = document.createElement("h4")
+    let btn = document.createElement("button")
+    btn.textContent = "Delete"
+    btn.classList.add("del")
+    h4phone.innerHTML = `Phone: ${x.phone}`
+    h4email.innerHTML = `Email: ${x.email}`
+    h3.innerHTML = `Name: ${x.firstName} ${x.lastName}`
+    div.id = state.state.contacts.length - 1
+    btn.id = state.state.contacts.length - 1
+    btn.addEventListener("click", (e) => {
+        let x = btn.parentNode
+        console.log(x)
+        if (e.target.id == btn.id) {
+            constacts.removeChild(x)
+            store.dispatch(deleteContact(x.id))
+        }
+    })
+    div.appendChild(h3)
+    div.appendChild(h4email)
+    div.appendChild(h4phone)
+    div.appendChild(btn)
+    contactDiv.appendChild(div)
+}
+
+store.subscribe(() => console.log(store.getState()))
+
+const addContactForm = document.getElementById("add-contact")
+addContactForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    store.dispatch(addContact({
+        firstName: addContactForm.firstName.value,
+        lastName: addContactForm.lastName.value,
+        email: addContactForm.email.value,
+        phone: addContactForm.phone.value,
+        id: store.getState().state.contacts.length
+    }))
+    renderWebPage()
+})
